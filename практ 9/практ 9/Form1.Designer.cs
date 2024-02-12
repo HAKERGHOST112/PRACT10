a0b6624d47f7207ca78bdd3e4169a13e22a9d342
GHOST <luchshevdanil005@gmail.com>
2024-02-12 13:41:13 +0300

    pract10

---
+++ практ 9/практ 9/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace практ_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(196, 420);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(379, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавление";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(379, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(192, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Удаление";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(379, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 37);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Редактирование";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(379, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(192, 37);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Найти среднию";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(376, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Средний год выпуска";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(589, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(379, 296);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(192, 37);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Запись в файла";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(379, 339);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(192, 37);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Чтение из файла";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ практ 9/практ 9/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace практ_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                string FIO = f.textBox1.Text;
+                int Age = Convert.ToInt32(f.textBox2.Text);
+                int Probeg = Convert.ToInt32(f.textBox3.Text);
+                people p = new people(FIO, Age, Probeg);
+                listBox1.Items.Add(p);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Выберите объект для редактирования");
+            }
+            else
+            {
+                Form2 f = new Form2();
+                people p = listBox1.Items[listBox1.SelectedIndex] as people;
+                f.textBox1.Text = p.FIO;
+                f.textBox2.Text = p.Age.ToString();
+                f.textBox3.Text = p.Probeg.ToString();
+                if (f.ShowDialog() == DialogResult.OK)
+                {
+                    string myFio = f.textBox1.Text;
+                    int myAge = Convert.ToInt32(f.textBox2.Text);
+                    int myProbeg = Convert.ToInt32(f.textBox3.Text);
+                    p.FIO = myFio;
+                    p.Age = myAge;
+                    p.Probeg = myProbeg;
+                    listBox1.Items[listBox1.SelectedIndex] = p;
+                }
+            }
+
+        }
+    
+
+        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double c = 0;
+            for(int i = 0; i < listBox1.Items.Count; i++)
+            {
+                people p = listBox1.Items[i] as people;
+                double a = p.Age;
+                c = c + a;
+            }
+            c=c/listBox1.Items.Count;
+            label2.Text = c.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("ОБЬЕКТ НЕ ВЫБРАН");
+            }
+            else
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+               StreamWriter streamWriter = new StreamWriter(saveFileDialog1.FileName);
+                for (int i = 0;i < listBox1.Items.Count;i++)
+                {
+                    people p = listBox1.Items[i] as people;
+                    streamWriter.WriteLine(p.FIO);
+                    streamWriter.WriteLine(p.Age);
+                    streamWriter.WriteLine(p.Probeg);
+                }
+                streamWriter.Close();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+            
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+               listBox1.Items.Clear();
+                StreamReader streamReader=new StreamReader(openFileDialog1.FileName);
+                while(streamReader.EndOfStream==false)
+                {
+                    people p=new people();
+                    p.FIO = streamReader.ReadLine();
+                    p.Age = Convert.ToInt32 (streamReader.ReadLine());
+                    p.Probeg= Convert.ToInt32(streamReader.ReadLine());
+                    listBox1.Items.Add(p);
+                }
+                streamReader.Close();
+
+            }
+           
+        }
+    }
+}
